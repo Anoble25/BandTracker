c8913305863f3f0612f42b47a8880e89d73146f4
Alex Noble <AWGR-Alex@comcast.net>
2018-05-11 14:33:28 -0700

    add .sql files

---
+++ band_tracker.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 11, 2018 at 09:32 PM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `band_tracker`
+--
+CREATE DATABASE IF NOT EXISTS `band_tracker` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `band_tracker`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bands`
+--
+
+CREATE TABLE `bands` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bands`
+--
+
+INSERT INTO `bands` (`id`, `name`) VALUES
+(10, 'Beach House'),
+(11, 'Arctic Monkeys'),
+(12, 'Tame Impala'),
+(13, 'Kendrick Lamar'),
+(14, 'Deerhunter'),
+(15, 'Blood Orange'),
+(17, 'Porches');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venues`
+--
+
+CREATE TABLE `venues` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `venues`
+--
+
+INSERT INTO `venues` (`id`, `name`) VALUES
+(11, 'Neumo\'s'),
+(12, 'Crocodile Cafe'),
+(13, 'Sunset Tavern'),
+(14, 'Tractor Tavern'),
+(15, 'Neptune Theatre'),
+(16, 'Moore Theatre'),
+(17, 'Tacoma Dome');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venues_bands`
+--
+
+CREATE TABLE `venues_bands` (
+  `id` int(11) NOT NULL,
+  `venue_id` int(11) NOT NULL,
+  `band_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `venues_bands`
+--
+
+INSERT INTO `venues_bands` (`id`, `venue_id`, `band_id`) VALUES
+(1, 1, 4),
+(2, 2, 5),
+(4, 8, 8),
+(6, 11, 10),
+(7, 13, 11),
+(8, 12, 11),
+(9, 14, 13),
+(10, 14, 14),
+(11, 14, 15);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bands`
+--
+ALTER TABLE `bands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venues`
+--
+ALTER TABLE `venues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venues_bands`
+--
+ALTER TABLE `venues_bands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bands`
+--
+ALTER TABLE `bands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `venues`
+--
+ALTER TABLE `venues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `venues_bands`
+--
+ALTER TABLE `venues_bands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
